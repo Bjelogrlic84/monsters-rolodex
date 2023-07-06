08a482325c4610b413276e79226415ce54a59f12
Bjelogrlic84 <bjelogrlic.ivan.84@gmail.com>
2023-07-06 10:25:20 +0200

    //

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 
 import Card from "../card/card.component";
-import "./card-list.styles.css"; // Uključite CSS fajl sa stilovima
+import "./card-list.styles.css"; // Uključivanje CSS fajla sa stilovima
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
-    
+
     return (
-      <div className="card-list"> {/* Dodajte className atribut */}
-        {monsters.map((monster) => {
-          
+      <div className="card-list"> {/* Dodajte atribut className */}
+        {monsters.map((monster) => { 
+          {/*Korišćenje metode `map()` nad `monsters` nizom 
+          kako bi se prikazala `Card` komponenta za svako čudovište.*/}
           return (
-          <Card monster={monster} />
-        );
+            <Card monster={monster} /> // Prikazivanje komponente Card za svako čudovište
+          );
         })}
       </div>
     );
